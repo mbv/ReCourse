564295b8279a16f3ef890629dffbf2d5da0081c9
Andrey Tatarenko <andrey17076@gmail.com>
2017-04-25 01:02:44 +0300

    Change project structure

--- src/main/webapp/app/applicationStates.js
+++ src/main/webapp/app/applicationStates.js
@@ -4,14 +4,18 @@ angular
 
 function AppStates($stateProvider, $urlRouterProvider) {
     [
-        { name: 'home', url: '/', templateUrl: 'templates/home.html' },
-        { name: 'login', url: '/login', templateUrl: 'templates/login.html' },
+        { name: 'root', url: '/' },
+        { name: 'home', url: '/home', templateUrl: 'templates/home.html' },
+        { name: 'signIn', url: '/sign_in', templateUrl: 'templates/sign_in.html' },
+        { name: 'signUp', url: '/sign_up', templateUrl: 'templates/sign_up.html' },
         { name: 'about', url: '/about', templateUrl: 'templates/about.html' },
-        { name: 'users',  url: '/users', templateUrl: 'templates/users/index.html' },
-        { name: 'courses',  url: '/courses', templateUrl: 'templates/courses/index.html' },
+        { name: 'crud', url: '/crud', templateUrl: 'templates/crud/index.html' },
+        { name: 'crud.users',  url: '/users', templateUrl: 'templates/crud/users/index.html' },
+        { name: 'crud.courses',  url: '/courses', templateUrl: 'templates/crud/courses/index.html' },
         { name: 'otherwise', url: '/otherwise', template: '<h1>None</h1><p>Nothing has been selected</p>' }
     ].forEach(function(state) { $stateProvider.state(state) });
 
     $urlRouterProvider.when('', '/');
+    $urlRouterProvider.when('/', '/home');
     $urlRouterProvider.otherwise('/otherwise');
 }
