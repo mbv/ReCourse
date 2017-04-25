801be68fd0aacccff82f2a50ee2ec4e41be23595
Andrey Tatarenko <andrey17076@gmail.com>
2017-04-26 01:21:49 +0300

    Move webapp congigs to separate folder.

--- src/main/webapp/app/application.js
+++ src/main/webapp/app/application.js
@@ -1,6 +1,7 @@
 angular
     .module('app', [
         'ui.router',
+        'ui.router.redirect',
         'ngResource',
         'ngSanitize',
         'ngCookies',
@@ -8,33 +9,4 @@ angular
         'ngMaterial',
         'angular-loading-bar',
         'md.data.table'
-    ])
-    .config(UnauthorizedRequestHandler)
-    .run(InitAuthenticationService);
-
-
-function UnauthorizedRequestHandler($httpProvider) {
-    $httpProvider.interceptors.push(interceptor);
-
-    function interceptor($injector) {
-        return {
-        responseError: function(response) {
-            if (response.status === 401) {
-                // $injector.get('AuthService').checkAuthorization();
-            }
-        }
-    };
-  }
-}
-
-function InitAuthenticationService(AuthService, $rootScope, $state) {
-  $rootScope.$on('$stateChangeStart', function(event, toState) {
-    if (!AuthService.isAuthorized) {
-      if (toState.name !== 'signIn' && toState.name !== 'signUp') {
-        event.preventDefault();
-        $state.go('signIn');
-      }
-    }
-  });
-  AuthService.checkAuthorization();
-}
+    ]);
--- src/main/webapp/app/configs/states.config.js
+++ src/main/webapp/app/configs/states.config.js
@@ -4,18 +4,19 @@ angular
 
 function AppStates($stateProvider, $urlRouterProvider) {
     [
-        { name: 'root', url: '/' },
+        { name: 'root', url: '/', redirectTo: 'home' },
         { name: 'home', url: '/home', templateUrl: 'templates/home.html' },
         { name: 'signIn', url: '/sign_in', templateUrl: 'templates/sign_in.html' },
         { name: 'signUp', url: '/sign_up', templateUrl: 'templates/sign_up.html' },
         { name: 'about', url: '/about', templateUrl: 'templates/about.html' },
-        { name: 'crud', url: '/crud', templateUrl: 'templates/crud/index.html' },
+        { name: 'crud', url: '/crud', redirectTo: 'crud.users', templateUrl: 'templates/crud/index.html' },
         { name: 'crud.users',  url: '/users', templateUrl: 'templates/crud/users/index.html' },
         { name: 'crud.courses',  url: '/courses', templateUrl: 'templates/crud/courses/index.html' },
-        { name: 'otherwise', url: '/otherwise', template: '<h1>None</h1><p>Nothing has been selected</p>' }
+        // { name: 'crud.courses',  url: '/courses', templateUrl: 'templates/crud/courses/index.html' },
+        { name: 'crud.lessons',  url: '/lessons', templateUrl: 'templates/crud/lessons/index.html' },
+        { name: 'otherwise', url: '/otherwise', template: '<h1>404</h1>' }
     ].forEach(function(state) { $stateProvider.state(state) });
 
     $urlRouterProvider.when('', '/');
-    $urlRouterProvider.when('/', '/home');
     $urlRouterProvider.otherwise('/otherwise');
 }
