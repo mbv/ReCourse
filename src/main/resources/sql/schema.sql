dbd6cdbeaa9e19b7fcce2c6c037f3fb5bd2d9266
Mikhail Snitavets <mksn13@gmail.com>
2017-04-28 00:51:20 +0300

    Add some mock data

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -18,7 +18,6 @@ CREATE TABLE `user` (
   `gender`        ENUM ('MALE', 'FEMALE') NOT NULL,
   `birthday`      DATE                                              DEFAULT NULL,
   `role`          ENUM ('STUDENT', 'TEACHER', 'ADMIN', 'DISABLED')  DEFAULT NULL,
-  `is_deleted`    BOOL                    NOT NULL                  DEFAULT FALSE,
   PRIMARY KEY (`id`),
   KEY `IDX_name` (`name`, `surname`),
   UNIQUE KEY `email_UNIQUE` (`email`)
@@ -243,38 +242,3 @@ INSERT INTO `oauth_client_details` VALUES (
                NULL,
                NULL
 );
-
--- -----------------------------------------------------------
--- User data
--- -----------------------------------------------------------
-INSERT INTO `user` (email, password_hash, name, surname, gender, birthday, role) VALUES
-  ('Ivan_Shimko@triumgroup.com',
-   '$2a$12$5EeGW2rSJVdrg4g8bp9g0exNjaGsjHNAq0BkR6UTU5baFr8Y3i522', /* 12345 */
-   'Ivan', 'Shimko', 'male', NULL, 'teacher'),
-  ('Andrey_Tatarenko@triumgroup.com',
-   '$2a$12$eI7e3tSsXOBx5ZDUxY4gAeZQTu7yxKH7fGmPrVindUoJPDz4Cp03G', /* 54321 */
-   'Andrey', 'Tatarenko', 'male', NULL, 'student'),
-  ('Mikhail_Snitavets@triumgroup.com',
-   '$2a$12$dhZ.l.x3RwbAYSMTOS.ERuRlRy6ikeSVIkdDPbD20uGrP08WmmZRe', /* 09876 */
-   'Mikhail', 'Snitavets', 'male', NULL, 'student'),
-  ('admin@triumgroup.com',
-  '$2a$12$dhZ.l.x3RwbAYSMTOS.ERuRlRy6ikeSVIkdDPbD20uGrP08WmmZRe', /* 09876 */
-  'Foo', 'Bar', 'male', NULL, 'admin');
-
--- -----------------------------------------------------------
--- Course data
--- -----------------------------------------------------------
-INSERT INTO `recourse`.`course` (title, description, status, max_students) VALUES
-  ('Awesome Java Course',
-   'Get out of your fucking mind with our java course',
-   'ONGOING',
-   20);
-
-INSERT INTO `recourse`.`lesson` (start_time, duration, course_id, topic, teacher_id, task) VALUES
-  (date('2016-01-01'),
-   time('5:00:00'),
-   (select `id` from `recourse`.`course` limit 1),
-   'Java Spring',
-   (select `id` from `recourse`.`user` where `role` = 'TEACHER' limit 1),
-   'Very long long long long long long long long long long long long long text.'
-  );
