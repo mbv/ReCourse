b76a9dfb7490d7790ba4b46b5e06156218a1d42a
Andrey Tatarenko <andrey17076@gmail.com>
2017-05-01 21:09:43 +0300

    Add oauth errors handling

--- src/main/webapp/app/configs/httpRequestErrorsHandler.config.js
+++ src/main/webapp/app/configs/httpRequestErrorsHandler.config.js
@@ -8,7 +8,13 @@ function HttpRequestErrorsHandler($httpProvider) {
     function interceptor($q, $rootScope) {
         return {
             responseError: function(response) {
-                $rootScope.$broadcast('httpError',response.data.errors);
+                var errors = [];
+                if (!!response.data.errors) {
+                    errors.concat(response.data.errors);
+                } else if (!!response.data.error_description) {
+                    errors.push({ title: 'Auth', message: response.data.error_description })
+                }
+                $rootScope.$broadcast('httpError', errors);
                 return $q.reject(response);
             }
         };
